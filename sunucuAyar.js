c8b58177ee00df84880b7cb76d6b89cb687b6653
Glitch (squats-bot) <none>
2020-12-22 14:31:30 +0000

    🚈🌎 Checkpoint ./sunucuAyar.js:1915850/572 ./ayarlar.json:1915850/274 ./main.js:1915850/95

--- sunucuAyar.js
+++ sunucuAyar.js
@@ -1,13 +1,13 @@
 const ayarlar = {
   guildID: '786918619397357588', // sunucu idsi
-  ownerRole:"", // sunucu sahiplerinin rolü
+  ownerRole:"788355567210201098", // sunucu sahiplerinin rolü
   teyitsizRolleri:[], // kayıt olmayanlara verieln rol
   jailRolu:"", // cezalılara verilen rol
   enAltYetkiliRolu:[], // ilk yetkili permi
-  activity:"", // botun durumu
+  activity:"Muhammed Demirel", // botun durumu
   status:"idle",
   invitelink:"", //davet linki
   symbol:"", // tag
-  voicechannel:"" // botun gireceği ses kanalı
+  voicechannel:"788821784123801660" // botun gireceği ses kanalı
 };
 module.exports = ayarlar;
