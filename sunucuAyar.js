aa5b097f16ca4d34fc1f9553e4dc7ea63f1b8968
Glitch (squats-bot) <none>
2020-12-22 11:13:07 +0000

    🚕🍟 Checkpoint ./sunucuAyar.js:1915850/180

--- sunucuAyar.js
+++ sunucuAyar.js
@@ -1,6 +1,6 @@
 const ayarlar = {
   guildID: '790589745759125555', // sunucu idsi
-  ownerRole:"785528282065928203", // sunucu sahiplerinin rolü
+  ownerRole:"788355567210201098", // sunucu sahiplerinin rolü
   teyitsizRolleri:[], // kayıt olmayanlara verieln rol
   jailRolu:"", // cezalılara verilen rol
   enAltYetkiliRolu:[], // ilk yetkili permi
@@ -8,6 +8,6 @@ const ayarlar = {
   status:"idle",
   invitelink:"", //davet linki
   symbol:"", // tag
-  voicechannel:"" // botun gireceği ses kanalı
+  voicechannel:"789017310920441887" // botun gireceği ses kanalı
 };
 module.exports = ayarlar;
